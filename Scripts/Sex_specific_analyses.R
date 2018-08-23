3060cfcd1556b0c8ad789bb9e0e364007cdade37
WillDuke <Will@WillIams-MacBook-Pro-3.local>
2018-08-23 16:52:46 -0400

    removed unused scripts, renamed for clarity, added documentation

--- Scripts/Sex_specific_analyses.R
+++
@@ -1,8 +0,0 @@
-require(tidyverse)
-load("R_data/allmol_noNAs.rda")
-molwithsex <- allmol_noNAs %>% 
-  mutate(Sex = c("M", "M", "F", "F", "M","M","M","M","F","F"))
-
-malesWT <- molwithsex %>% filter(Sex == "M") %>% 
-females <- molwithsex %>% filter(Sex == "F")
-
--- Scripts/splsda.R
+++ Scripts/splsda.R
@@ -8,7 +8,7 @@ load("R_data/allmol_noNAs.rda")
 # ***for predict: train = c(sample(seq(1,9,2), 3), sample(seq(2,10,2), 3))
 x <- as.matrix(allmol_noNAs[,-1])
 y <- allmol_noNAs$VF
-#create logistic model using logistic (alt: LDA)
+#create logistic model using logistic (alt: LDA) (also used K = 1 in powerpoint)
 fit <- splsda(x, y, K = 2, eta = 0.9, kappa=0.5,
               classifier=c('logistic'), scale.x=TRUE)
 #print variable selection
